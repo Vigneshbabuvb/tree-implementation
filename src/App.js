8737aaad621325680f324934b029fc31a60118c6
Vigneshbabuvb <vigneshbabu35@gmail.com>
2022-02-22 07:55:45 +0530

    Changed the uuid generator function code.

--- src/App.js
+++ src/App.js
@@ -78,9 +78,13 @@ function App() {
 
   // Helper function to generate random UUID for root key
   function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c==='x' ? r :((r&0x3)|0x8)).toString(16);
+    });
+    return uuid;
   }
 
   function renderChildTree(treeNodeCollection) {
